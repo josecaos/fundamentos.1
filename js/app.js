79401218934e9137d672e1c6b6f695069bb6318c
josecaos <jazzvoon@gmail.com>
2018-11-23 15:50:11 -0600

    Clase 9

---
+++ js/app.js
@@ -0,0 +1,65 @@
+// jQuery = non conflict mode
+// $ = conflict mode
+// inicializacion
+// jQuery(document)
+$(document).ready( () => {
+
+  // alert("ALERTAAAAA!! VAMOS A MORIRRR")
+
+  // alert(suma(10,10))
+  // alert(suma(10,10) + mul(10,10))
+  // alert(mul(suma(10,10),10))
+
+imprime(mul(3,9))
+regresa()
+fondo('purple')
+ancho()
+
+
+} )
+
+
+// funciones estaticas
+function suma(valor1=1,valor2=1) {
+  // console.log( 100 + 200)
+  return valor1 + valor2
+}
+
+function mul(valor1,valor2=10) {
+
+  return valor1*valor2
+}
+
+function imprime(parametro) {
+
+  return $('.participantes_item_text h2').html(parametro)
+  // return $('.participantes_item_text h2').append(paramet$('.participantes_item_text h4').html()
+
+}
+
+function regresa() {
+
+  var x = $('.participantes_item_text h4').html()
+
+  return alert(x)
+
+}
+
+function fondo(color) {
+
+  $('body').css('background-color', color)
+  // $('body').css({'':''})
+
+}
+
+function ancho() {
+  // eventos
+  $(window).on('resize', ()=>{
+
+    var x = $(document).width()
+    // console.log("RESIZE")
+    console.log(x)
+
+  })
+
+}
