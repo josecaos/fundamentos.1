82aec5d9be4a2eee03e9c2a118be0bf459cd0bad
josecaos <jazzvoon@gmail.com>
2018-12-07 13:54:37 -0600

    Ejercicio agregar dependencias

--- js/app.js
+++ js/app.js
@@ -14,9 +14,12 @@ $(document).ready( () => {
 // regresa()
 // fondo('lime')
 // ancho()
+imgLiquid()
+colores_fondo(1000,["#3465fa","purple","black","yellow","lime","orange"])
 
-colores_fondo(250,["#3465fa","purple","black","yellow","lime","orange"])
+// Scripts tema
 
+//
 } )
 
 
@@ -80,3 +83,22 @@ function colores_fondo(time=1000,colorArray=['purple','red']) {
   },time)
 
 }
+
+// Scripts tema
+// control de imagenes
+function imgLiquid() {
+
+  $('.imgLiquid.imgLiquidFill').imgLiquid()
+  $('.imgLiquid.imgLiquidNoFill').imgLiquid({
+    fill:false
+  })
+  $('.imgLiquid.imgLiquidNoFillLeft').imgLiquid({
+    fill:false,
+    horizontalAlign:'left'
+  })
+  $('.imgLiquid.imgLiquidNoFillRight').imgLiquid({
+    fill:false,
+    horizontalAlign:'right'
+  })
+}
+//
